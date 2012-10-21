0dd560d54c81a7060599461d499ccbc7ace2c731
Pixelastic <tim@pixelastic.com>
2012-10-21 13:08:15 +1300

    Converting horst to a Class.

--- tests/horst.test.rb
+++ tests/horst.test.rb
@@ -5,35 +5,35 @@ require '../horst'
 class HorstTests < Test::Unit::TestCase
 
 	def test_timing_line?
-		assert_equal(false, timing_line?("1"))
-		assert_equal(true, timing_line?("00:00:28,572 --> 00:00:33,237"))
-		assert_equal(false, timing_line?("<i>Quand on prie la bonne étoile</i>"))
+		assert_equal(false, Horst::timing_line?("1"))
+		assert_equal(true, Horst::timing_line?("00:00:28,572 --> 00:00:33,237"))
+		assert_equal(false, Horst::timing_line?("<i>Quand on prie la bonne étoile</i>"))
 	end
 
 	def test_split_timing_line
 		input = "00:00:28,572 --> 00:00:33,237"
 		expected = ["00:00:28,572", "00:00:33,237"]
-		assert_equal(expected, split_timing_line(input))
+		assert_equal(expected, Horst::split_timing_line(input))
 	end
 
 	def test_join_timing
 		input = ["00:00:28,572", "00:00:33,237"]
 		expected = "00:00:28,572 --> 00:00:33,237"
-		assert_equal(expected, join_timing(input[0], input[1]))
+		assert_equal(expected, Horst::join_timing(input[0], input[1]))
 	end
 
 	def test_timing_to_timestamp
-		assert_equal(572, timing_to_timestamp("00:00:00,572"))
-		assert_equal(28572, timing_to_timestamp("00:00:28,572"))
-		assert_equal(88572, timing_to_timestamp("00:01:28,572"))
-		assert_equal(3688572, timing_to_timestamp("01:01:28,572"))
+		assert_equal(572, Horst::timing_to_timestamp("00:00:00,572"))
+		assert_equal(28572, Horst::timing_to_timestamp("00:00:28,572"))
+		assert_equal(88572, Horst::timing_to_timestamp("00:01:28,572"))
+		assert_equal(3688572, Horst::timing_to_timestamp("01:01:28,572"))
 	end
 
 	def test_timestamp_to_timing
-		assert_equal("00:00:00,572", timestamp_to_timing(572))
-		assert_equal("00:00:28,572", timestamp_to_timing(28572))
-		assert_equal("00:01:28,572", timestamp_to_timing(88572))
-		assert_equal("01:01:28,572", timestamp_to_timing(3688572))
+		assert_equal("00:00:00,572", Horst::timestamp_to_timing(572))
+		assert_equal("00:00:28,572", Horst::timestamp_to_timing(28572))
+		assert_equal("00:01:28,572", Horst::timestamp_to_timing(88572))
+		assert_equal("01:01:28,572", Horst::timestamp_to_timing(3688572))
 	end
 
 	def test_change_speed
@@ -41,6 +41,6 @@ class HorstTests < Test::Unit::TestCase
 		initial_speed = 23.976
 		new_speed = 25
 		expected = "00:00:27,401"
-		assert_equal(expected, change_speed(input, initial_speed, new_speed))
+		assert_equal(expected, Horst::change_speed(input, initial_speed, new_speed))
 	end
 end
