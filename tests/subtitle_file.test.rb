2edcdc2328810c660da0f690ebc4a3bb23716c15
Pixelastic <tim@pixelastic.com>
2013-02-04 15:32:44 +0100

    Fix path relativness in test data files.

--- tests/subtitle_file.test.rb
+++ tests/subtitle_file.test.rb
@@ -5,12 +5,16 @@ require_relative '../subtitle_file'
 class SubtitleFileTest < Test::Unit::TestCase
 
 	def setup
-		@input_file = "./data/input.srt"
+		dirname = File.dirname(__FILE__)
+		@datadir = File.join(dirname, 'data')
+		@input_file = File.join(@datadir, "input.srt")
+		@bogus_file = File.join(@datadir, 'bogus.srt')
+		@expected_file = File.join(@datadir, 'expected.srt')
 	end
 
 	def test_input_file_not_found
 		assert_raise Errno::ENOENT do
-			SubtitleFile.new("./bogus.srt")
+			SubtitleFile.new(@bogus_file)
 		end
 	end
 
@@ -21,7 +25,7 @@ class SubtitleFileTest < Test::Unit::TestCase
 	end
 
 	def test_change_speed
-		expected = File.open('./data/expected.srt').read
+		expected = File.open(@expected_file).read
 		input = SubtitleFile.new(@input_file).change_speed(23.946, 25)
 		assert_equal(expected, input, "expected.srt does not match changed speed")
 	end
